ce9b758bd67b7e5ffc25704cbc62f59271b05947
rachfuerst <rach.fuerst@gmail.com>
2019-11-24 09:38:01 -0600

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,17 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  def edit
+    # Have the #edit method store the article record in an instance variable
+    @article = Article.find(params[:id])
+  end
+   
+  def update
+    # Find the article that matches the params id
+    @article= Article.find(params[:id])
+    # Update the article according to what's input in the params
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    #Redirect the user to the show page so they can see the updated record.
+    redirect_to article_path(@article)
+  end
 end
